1788464b906292e96e6f94d1a514d724d9c912d9
Tim Menzies <tim.menzies@gmail.com>
2020-06-02 00:42:25 -0400

    saving

---
+++ src/row.lua
@@ -0,0 +1,18 @@
+local lib = require "lib"
+local Row = lib.class()
+
+function Row:_init(a) 
+  self.cells = a.cells and a.cells or a
+end
+
+function Row:dist(other,cols,    p,    d,n,inc)
+  d, n, p = 0, 0.0001, p or 2
+  for _,c in pairs(cols) do
+    inc = c:dist(self.cells[c.pos], other.cells[c.pos])
+    d   = d + inc^p
+    n   = n + 1
+  end
+  return (d/n)^(1/p) 
+end
+
+return Row
